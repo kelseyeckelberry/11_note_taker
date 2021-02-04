5a929f1b42917e0d5a95016bf583c583ede381ad
kelseck <kelseyeckelberry@gmail.com>
2021-02-03 22:07:42 -0600

    server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log('App listening on PORT: ' + PORT);
+  });
+  
