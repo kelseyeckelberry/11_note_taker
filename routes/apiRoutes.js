2d4448687f36afc522f791828ba9426f38fc1ec3
kelseck <kelseyeckelberry@gmail.com>
2021-02-04 23:51:00 -0600

    post + delete...still not working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,42 @@
-const savedNotes = require("../db/db.json");
+const fs = require('fs');
 
 module.exports = function (app) {
-  app.get("/api/notes", (req, res) => {
-    fs.readFile("../db/db.json", (err, data) => {
+  app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) throw err;
-      var savedNotes = JSON.parse(data);
-      res.json(savedNotes);
+      res.json(JSON.parse(data));
     });
   });
-//   app.post("/api/notes", (req, res) => {
-//     var newNote = {
-//       id: id,
-//       title: req.body.title,
-//       text: req.body.text,
-//     };
-//     fs.readFile("../db/db.json", (err, data) => {
-//       if (err) throw err;
-//       var savedNotes = JSON.parse(data);
-//       savedNotes.push(newNote);
-//       fs.writeFile("../db/db.json", JSON.stringify(savedNotes), (err) => {
-//         if (err) throw err;
-//       });
-//     });
-//   });
-  app.delete("/api/notes/:id", (req, res) => {
-    var notes = JSON.parse(fs.readFileSync("../db/db.json", "utf-8"));
-    var newNote = notes.filter(note => note.id !== req.params.id);
-    fs.writeFileSync("../db/db.json", JSON.stringify(newNote));
-    res.json(true);
+
+  app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      let savedNote = JSON.parse(data);
+      let id = savedNote.length + 1;
+      let newNote = JSON.stringify(req.body);
+      newNote = JSON.parse(newNote);
+      savedNote.push({ ...newNote, id: id });
+      savedNote = JSON.stringify(savedNote);
+      fs.writeFile('../db/db.json', savedNote, (err, data) => {
+        res.sendStatus(200);
+      });
+    });
+  });
+
+  app.delete('/api/notes/:id', (req, res) => {
+    let id = req.params.id;
+    console.log(id);
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      let deleteNote = JSON.parse(data);
+      console.log(deleteNote);
+      deleteNote = deleteNote.filter((notes) => notes.id != id);
+      console.log(deleteNote);
+      deleteNote - JSON.stringify(deleteNote);
+      fs.writeFile('./db/db.json', deleteNote, (err, data) => {
+        if (err) throw err;
+        res.send(deleteNote);
+      });
+    });
   });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,15 @@
-const path = require("path");
+var path = require('path');
 
 module.exports = function (app) {
-  app.get("/notes", (_req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
-  app.get("/", (_req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 };
